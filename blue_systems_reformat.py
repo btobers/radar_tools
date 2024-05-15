5c1a14e61594b2f4eb38859bf66b4bf8392cb3b2
btobers <tobers.brandon@gmail.com>
2024-05-14 21:20:42 -0400

    update

--- blue_systems_reformat.py
+++ blue_systems_reformat.py
@@ -5,7 +5,8 @@ Restructure radar sounding data from Laurent Mingo's Blue Systems Air-IPR.
 import xml.etree.ElementTree as ET
 import h5py
 import numpy as np
-import sys, glob, os, argparse
+import sys, glob, os, argparse, re
+from datetime import datetime, timedelta
 
 
 # Define a custom sorting function
@@ -13,17 +14,39 @@ def sort_by_numeric_value(s):
     return int(s.split('_')[1])  # Split the string by '_' and return the second part as an integer
 
 
-def parseHeader():
-    header = {}
+def dm2dd(dms):
+    return ((dms - dms % 100) / 100 + (dms % 100) / 60)
 
-    header["spt"] = 3200
-    header["fs"] = 1
+
+def _2dt(date_obj, ms):
+    # Convert milliseconds to seconds
+    sec = ms / 1000
+    # Calculate the datetime object for the given UTC time
+    utc_datetime = datetime.combine(date_obj, datetime.min.time()) + timedelta(seconds=sec)
+    return utc_datetime
+
+
+def _xmlGetVal(xml, name):
+    """
+    Borrowed from dlilien's impDAR: Look up a value in an XML fragment. Mod from Nat Wilson's irlib.
+    """
+    m = re.search(r'<Name>{0}</Name>[\r]?\n<Val>'.format(
+        name.replace(' ', r'\s')), xml, flags=re.IGNORECASE)
+    if m is not None:
+        tail = xml[m.span()[1]:]
+        end = tail.find('</Val')
+        return tail[:end]
+    else:
+        return None
+
+
+def parseHeader(dig_xml):
+    header = {}
+    header["stack"] = int(_xmlGetVal(dig_xml, 'Stacking'))
+    header["spt"] = int(_xmlGetVal(dig_xml, 'Record Length'))
+    header["fs"] = float(_xmlGetVal(dig_xml, ' Sample Rate'))
     header["pre_trig"] = 0
-    # header["pre_trig"] = struct.unpack("q", data[12:20])[0]
-    header["prf"] = 1000
-    header["stack"] = 1
-    # header["trig"] = struct.unpack("h", data[36:38])[0]
-    # header["fs"] = struct.unpack("d", data[38:46])[0]
+    header["prf"] = np.nan
 
     return header
 
@@ -56,50 +79,39 @@ def buildH5(header, rx, gps, outfile):
 
 
 def parse(fpath=''):
-    
-    # hardcode snum
-    snum = 3200
 
     # read in .h5 file
     with h5py.File(fpath, "r")  as fd:
+        # get date string from file as datetime obj
+        datestr = fpath.split('/')[-1].split(' ')[-1][:-3]
+        date_obj = datetime.strptime(datestr, "%b.%d.%Y") 
         lns = sorted(fd.keys(), key=sort_by_numeric_value)
         for ln in lns:
             line = fd[ln]
             # get header info 
-            header = parseHeader()
+            header = parseHeader(line[list(line.keys())[0]]["datacapture_0/echogram_0"].attrs['Digitizer-MetaData_xml'])
             # initialize data array
-            data = np.zeros((3200, len(line)))
+            data = np.zeros((header["spt"], len(line)))
 
             # initialize latitude and longitude lists variables to store values
             lats = []
             lons = []
             hgts = []
             times = []
-            dates = []
 
             # loop through traces
             for key in line.keys():
+
                 i = int(key.split("_")[1])
                 # fill in data array
                 data[:,i] = line[key]["datacapture_0/echogram_0"][:]
 
                 # parse gps xml data
-                gps_xml = ET.fromstring(line[key]["datacapture_0/echogram_0"].attrs['GPS Cluster- MetaData_xml'])
-                # Iterate over all <String> elements and get lat/lon for each trace
-                for string_elem in gps_xml.findall('./String'):
-                    name_elem = string_elem.find('Name')
-                    val_elem = string_elem.find('Val')
-                    if name_elem is not None and val_elem is not None:
-                        name = name_elem.text.strip()
-                        val = val_elem.text.strip()
-                        if name == 'Lat_N':
-                            lats.append(float(val)/100)
-                        elif name == 'Long_ W':  # Note the space in 'Long_ W'
-                            lons.append(float(val)/100*-1)
-                        elif name == 'Alt_asl_m': # Guessing this is in ref to WGS84 ellipsoid
-                            hgts.append(float(val))
-                        elif name == 'GPS_timestamp_UTC':
-                            times.append(float(val))
+                gps_xml = line[key]["datacapture_0/echogram_0"].attrs['GPS Cluster- MetaData_xml']
+                lats.append(dm2dd(float(_xmlGetVal(gps_xml, 'Lat_N'))))
+                lons.append(dm2dd(float(_xmlGetVal(gps_xml, 'Long_ W'))) * -1)
+                hgts.append(float(_xmlGetVal(gps_xml, 'Alt_asl_m')))
+                times.append(np.datetime64(_2dt(date_obj, float(_xmlGetVal(gps_xml, 'GPS_timestamp_UTC')))))
 
             fix =  {"lons": lons, "lats": lats, "hgts": hgts, "times": times}
             tnum = data.shape[1]
@@ -116,8 +128,7 @@ def parse(fpath=''):
                             fix["lons"][i],
                             fix["lats"][i],
                             fix["hgts"][i],
-                            np.nan,
-                            # np.datetime_as_string(fix["times"][i]),
+                            np.datetime_as_string(fix["times"][i]),
                         ]
                     )
                 gps = np.array(gps, dtype=gps_t)
@@ -144,11 +155,11 @@ def main():
     args = parser.parse_args()
 
     for fn in glob.glob(args.datapath + '/*.h5'):
-        # try:
-        parse(fn)
-        # except Exception as err:
-        #     print(err)
-        #     pass
+        try:
+            parse(fn)
+        except Exception as err:
+            print(f"Error on file {fn}: {err}")
+            pass
 
 # execute if run as a script
 if __name__ == "__main__":
